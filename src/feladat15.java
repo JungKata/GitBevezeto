f33b326507cfce36b83931d56e347ec77be0f2b3
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-13 16:17:44 +0200

    Feladat15 -25 elemű valós értékeket tartalmazo tömb

--- src/feladat15.java
+++ src/feladat15.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class feladat15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double [] tomb = new double[25];
+
     }
 }
