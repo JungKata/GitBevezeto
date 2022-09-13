1fe6daf03111ed337a36f4b419867138766bd9c3
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-13 15:46:47 +0200

    feladatok14 menupont, kiiratas

--- src/feladat14.java
+++ src/feladat14.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class feladat14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Kérem adjon meg egy szöveget: ");
+        String szoveg = sc.nextLine();
+        String manuPont = "";
+        while (!manuPont.equals("f"))
+        {
+            System.out.println("Adja meg mit szedretne csinálni a szöveggel: ");
+            System.out.println("\t a- Nagybetűsé alakítani");
+            System.out.println("\t b- Kisbetűssé alakítani");
+            System.out.println("\t c- Lekérdezni a hosszát");
+            System.out.println("\t d- Össze hasonlítani a másikkal");
+            System.out.println("\t e- A szöveg egy részét kiiratni");
+            System.out.println("\t f- Kilépés");
+            manuPont = sc.nextLine().toLowerCase();
 
+        }
     }
 }
