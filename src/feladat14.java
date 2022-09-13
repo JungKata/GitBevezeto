4b974cf135f22860f6f6cacd0d6f079364f75122
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-13 16:08:29 +0200

    feladatok14 menupont, kiiratas,kész

--- src/feladat14.java
+++ src/feladat14.java
@@ -16,6 +16,46 @@ public class feladat14 {
             System.out.println("\t e- A szöveg egy részét kiiratni");
             System.out.println("\t f- Kilépés");
             manuPont = sc.nextLine().toLowerCase();
+            switch (manuPont){
+                case "a":
+                    System.out.println(szoveg.toUpperCase());
+                    break;
+                      case "b":
+                          System.out.println(szoveg.toLowerCase());
+                    break;
+                      case "c":
+                          System.out.printf("A szoveg hossza %d karakter \n" , szoveg.length());
+                    break;
+                      case "d":
+                          System.out.println("Kérem adja meg a masik szöveget:");
+                          String szoveg2= sc.nextLine();
+                          int osszehasonlitas = szoveg.compareTo(szoveg2);
+                          if (osszehasonlitas < 0)
+                          {
+                              System.out.println("Az első szöveg van előrébb az ABC-ben");
+                          }else if(osszehasonlitas > 0 ){
+                              System.out.println("Második szöveg van előrébb az ABC-ben");
+                          }else {
+                              System.out.println("A két szöveg megegyezik");
+                          }
+
+                    break;
+                          case "e":
+                              System.out.println("Kérem adja meg a kezdő karakter sorszámát amit ki szeretne iratni: ");
+                              int a = sc.nextInt();
+                              System.out.println("Kérem adja meg a utolsó karakter sorszámát amit ki szeretne iratni: ");
+                              int b = sc.nextInt();
+                              System.out.println(szoveg.substring(a -1, b - 1));
+                    break;
+                          case "f":
+                              System.out.println("Viszlát");
+                    break;
+                default:
+                    System.out.println("Rossz lehetőséget adott meg, kérem a menüpontok közül válasszon!");
+                    break;
+
+
+            }
 
         }
     }
