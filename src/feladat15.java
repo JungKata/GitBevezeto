e93262ef943be50b9ffb0893aa84ed8c7d763b47
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-13 16:20:10 +0200

    Feladat15 -25 elemű valós értékeket tartalmazo tömb -véletlen értékekkel való feltöltés

--- src/feladat15.java
+++ src/feladat15.java
@@ -4,6 +4,10 @@ public class feladat15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double [] tomb = new double[25];
+        for (int i = 0; i < tomb.length; i++)
+        {
+        tomb[i] = Math.random() * 200 - 100;
+        }
 
     }
 }
